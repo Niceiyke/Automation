a7d9196e068dd182fa70679a653b10d5b47631dd
Niceiyke <74918186+Niceiyke@users.noreply.github.com>
2022-06-09 22:48:19 +0100

    new

--- GUI/calculator.py
+++ GUI/calculator.py
@@ -5,13 +5,14 @@ def create_window(theme):
     sg.theme(theme)
     sg.set_options(font= 'Franklin 12',button_element_size=(6,3))
     button_size=(5,3)
+    window_size=(10,4)
 
-    layout=[[sg.Text('Output',
+    layout=[[sg.Text('',
     font='Franklin 30',
     expand_x=True, 
     justification ='right',
     pad=(10,20),
-    right_click_menu =theme_menu )],
+    right_click_menu =theme_menu, key='-TEXT-')],
     [sg.Button('Clear',expand_x=True),sg.Button('Enter',expand_x=True)],
     [sg.Button('7',size=button_size),sg.Button('8',size=button_size),sg.Button('9',size=button_size),sg.Button('*',size=button_size)],
     [sg.Button('4',size=button_size),sg.Button('5',size=button_size),sg.Button('6',size=button_size),sg.Button('/',size=button_size)],
@@ -19,11 +20,14 @@ def create_window(theme):
     [sg.Button('0',expand_x=True),sg.Button('.',size=button_size),sg.Button('+',size=button_size)],
 
     ] 
-    return sg.Window('Calculator',layout)
+    return sg.Window('Calculator',layout,window_size)
 theme_menu= ['menu',['DarkGrey1','LightGrey2','Dark','random']]
 
 window= create_window("DarkGrey1")
 
+current_num =[]
+current_operation=[]
+new_result=[]
 while True:
     event, values =window.read()
     if event== sg.WIN_CLOSED:
@@ -35,16 +39,53 @@ while True:
 
 
     if event in ['1','2','3','4','5','6','7','8','9','0','.']:
-        print(event)
+        current_num.append(event)
+        num_string =''.join(current_num)
+        window['-TEXT-'].update(num_string)
+  
 
     if event in ['+','-','*','/']:
-        print(event)
+        if len(new_result)==0:
+            current_operation.append(''.join(current_num))
+            current_num=[]
+            current_operation.append(event)
+            window['-TEXT-'].update('')
+            print(current_operation)
+        else:
+            new_result.append(''.join(event))
+            print(new_result)
 
-    if event =='Clear':
-        print(event)
+        
 
     if event =='Enter':
-        print(event)
+        if len(new_result)==0:
+            current_operation.append(''.join(current_num))
+            result=eval(''.join(current_operation))
+            current_operation=[]
+            current_num=[]
+            new_result.append(str(result))
+            window['-TEXT-'].update(result)
+            print(new_result)
+        else:
+
+            new_result.append(''.join(current_num))
+            result =eval(''.join(new_result))
+            new_result=[]
+            current_num=[]
+            new_result.append(str(result))
+            window['-TEXT-'].update(result)
+          
+                
+            print(result)
+
+    
+
+    if event =='Clear':
+        current_operation=[]
+        current_num=[]
+        new_result=[]
+        window['-TEXT-'].update('')
+
 
 
 

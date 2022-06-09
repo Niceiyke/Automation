945003365d5f7b8f4ea91364c2a476ba0619cf40
Niceiyke <74918186+Niceiyke@users.noreply.github.com>
2022-06-09 18:28:54 +0100

    Update calculator.py

--- GUI/calculator.py
+++ GUI/calculator.py
@@ -1,28 +1,50 @@
 import PySimpleGUI as sg
 
-sg.theme('PythonPlus')
-sg.set_options(font= 'Franklin 12',button_element_size=(6,3))
-button_size=(5,3)
+def create_window(theme):
 
-layout=[[sg.Text('Output', font='Franklin 30',expand_x=True, justification ='right',pad=(10,20))],
-[sg.Button('Clear',key='-CLEAR-',expand_x=True),sg.Button('Enter',key='-Enter-',expand_x=True)],
-[sg.Button('7',key='-7-',size=button_size),sg.Button('8',key='-8-',size=button_size),sg.Button('9',key='-9-',size=button_size),sg.Button('*',key='-*-',size=button_size)],
-[sg.Button('4',key='-4-',size=button_size),sg.Button('5',key='-5-',size=button_size),sg.Button('6',key='-6-',size=button_size),sg.Button('/',key='-/-',size=button_size)],
-[sg.Button('1',key='-1-',size=button_size),sg.Button('2',key='-2-',size=button_size),sg.Button('3',key='-3-',size=button_size),sg.Button('-',key='---',size=button_size)],
-[sg.Button('0',key='-0-',expand_x=True),sg.Button('.',key='-.-',size=button_size),sg.Button('+',key='-+-',size=button_size)],
+    sg.theme(theme)
+    sg.set_options(font= 'Franklin 12',button_element_size=(6,3))
+    button_size=(5,3)
 
-] 
+    layout=[[sg.Text('Output',
+    font='Franklin 30',
+    expand_x=True, 
+    justification ='right',
+    pad=(10,20),
+    right_click_menu =theme_menu )],
+    [sg.Button('Clear',expand_x=True),sg.Button('Enter',expand_x=True)],
+    [sg.Button('7',size=button_size),sg.Button('8',size=button_size),sg.Button('9',size=button_size),sg.Button('*',size=button_size)],
+    [sg.Button('4',size=button_size),sg.Button('5',size=button_size),sg.Button('6',size=button_size),sg.Button('/',size=button_size)],
+    [sg.Button('1',size=button_size),sg.Button('2',size=button_size),sg.Button('3',size=button_size),sg.Button('-',size=button_size)],
+    [sg.Button('0',expand_x=True),sg.Button('.',size=button_size),sg.Button('+',size=button_size)],
 
+    ] 
+    return sg.Window('Calculator',layout)
+theme_menu= ['menu',['DarkGrey1','LightGrey2','Dark','random']]
 
-window=sg.Window('Calculator',layout)
+window= create_window("DarkGrey1")
 
 while True:
     event, values =window.read()
     if event== sg.WIN_CLOSED:
         break
 
+    if event in theme_menu[1]:
+        window.close()
+        window=create_window(event)
 
 
+    if event in ['1','2','3','4','5','6','7','8','9','0','.']:
+        print(event)
+
+    if event in ['+','-','*','/']:
+        print(event)
+
+    if event =='Clear':
+        print(event)
+
+    if event =='Enter':
+        print(event)
 
 
 
